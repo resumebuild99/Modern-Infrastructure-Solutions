858f4af338ffd987924d189d70f2671c485e8424
resumebuild99 <resume@git.ggu.im>
2026-08-13 15:21:53 +0800

    Terraform Day02 build

---
+++ terraform/day-02/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+  profile = "bootcamp"
+
+
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+
+  tags = {
+    Name        = "day-02-vpc"
+    Environment = "Lab"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name        = "day-02-public-subnet"
+    Environment = "Lab"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
